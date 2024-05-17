3664504fec1ecc93d357d6836ead4b28e6674ff0
ngoduc <ngoduc202@gmail.com>
2024-05-18 00:44:54 +0700

    create: first look UI

--- src/pages/HomePage/HomePart.jsx
+++ src/pages/HomePage/HomePart.jsx
@@ -1,14 +1,15 @@
-
-import Carousel from '../../components/Courousel/Carousel'
-import Content from '../../components/Content/Content'
+import Carousel from "../../components/Courousel/Carousel";
+import Content from "../../components/Content/Content";
+import ContentExtra from "../../components/ContentExpand/ContentExtra";
 
 function HomePart() {
   return (
     <div>
-        <Carousel></Carousel>
-        <Content></Content>
+      <Carousel></Carousel>
+      <ContentExtra></ContentExtra>
+      <Content></Content>
     </div>
-  )
+  );
 }
 
-export default HomePart
+export default HomePart;
