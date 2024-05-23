078e93d25a0d40bf11bf8a887d4393c9b1443430
ngoduc <ngoduc202@gmail.com>
2024-05-23 15:31:41 +0700

    some config

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "animate.css"
+
+import "animate.css";
 function Footer() {
   return (
-    <footer className="bg-dark text-white pt-5 pb-4 wow animate__animated animate__backInRight">
+    <footer className="bg-dark text-white pt-5 pb-4 wow animate__animated animate__fadeIn">
       <div className="container text-center text-md-left">
         {/*Đây là một chỗ*/}
         <div className="row text-center text-md-left">
