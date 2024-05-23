8148064381888575bdd2f3c818993c2c657c05f3
minhduy4167 <cocoloto997@gmail.com>
2024-05-23 11:38:25 +0700

    base login

--- src/components/Sub-header/SubHead.jsx
+++ src/components/Sub-header/SubHead.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './subhead.css';
 
 function SubHead() {
   return (
-    <div>SubHead</div>
-  )
+    <Container col4>
+      <Row>
+        <Col className='title-subhead'> Hãy nâng lên bản pro nào!</Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default SubHead
+export default SubHead;
