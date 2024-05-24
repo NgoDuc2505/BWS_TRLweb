a571a6691df8253bb3841bd7a21549997c4d76a0
ngoduc <ngoduc202@gmail.com>
2024-05-24 23:13:32 +0700

    set up redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import store from "./Redux/strore.js";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Fragment>
+    <Provider store={store}>
       <App />
+    </Provider>
   </Fragment>
 );
